cf46fa4c301c171238b492539294844454b74986
EtDuxDeus <45258139+EtDuxDeus@users.noreply.github.com>
2019-05-01 23:35:56 +0300

    Add files via upload

---
+++ laba5/var0.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StructLabStudent
+{
+    partial class Program
+    {
+        static void Variant0(Student[] studs)
+        {
+            StreamWriter strwr = new StreamWriter("output0.txt");
+            for (int i = 0; i < studs.Length; i++)
+            {
+                if (studs[i].sex == 'F' && studs[i].informaticsMark == '5')
+                {
+                    strwr.Write(studs[i].surName);
+                    strwr.WriteLine();
+                }
+            }
+            strwr.Close();
+        }
+    }
+}
